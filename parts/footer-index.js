693ee799cc7364af69bce44b0fac5e6d358a6acd
sotonoro <ireriren@gmail.com>
2024-07-08 19:30:05 +0300

    add code

---
+++ parts/footer-index.js
@@ -0,0 +1,22 @@
+var footer = `<footer>
+<div class="footer-upper">
+  <h2 class="footer-text" id="footer-title">Начните творить прямо сейчас</h2>
+  <img src="assets/images/Logo-2.png" id="footer-logo" width="200px" height="50px"></img>
+  <div class="social-media">
+    <a href="https://m.facebook.com/login/?locale2=ru_RU"><img src="assets/images/facebook.png" width="30px"
+        height="30px"></a>
+    <a href="https://www.pinterest.ru/login/"><img src="assets/images/pinterest.png" width="30px"
+        height="30px"></a>
+    <a href="https://www.instagram.com/accounts/login/"><img src="assets/images/instagram.png" width="30px"
+        height="30px"></a>
+    <a href="https://www.youtube.com/account"><img src="assets/images/youtube.png" width="30px" height="30px"></a>
+  </div>
+</div>
+<div class="footer-bottom">
+  <a href="pages/Support.html" class="text-footer-bottom">Поддержать нас</a>
+  <a href="policy.pdf" class="text-footer-bottom">Политика безопасности</a>
+</div>
+</footer>
+</div>`
+
+document.body.insertAdjacentHTML("beforeend", footer);
---
+++ parts/navigation-index.js
@@ -0,0 +1,86 @@
+
+var navbar = `<header class="header">
+<nav class="navbar">
+  <div class="nav-wrapper">
+    <a href="Index.html"><div class="nav-name"></div></a>
+    <div class="hamburger">
+      <span class="bar" style="background-color: #FFB27B"></span>
+      <span class="bar" style="background-color: #6F4D35"></span>
+      <span class="bar" style="background-color: #20150D"></span>
+    </div>
+  </div>
+  <div class="nav-menu">
+    <div class="nav-column">
+      <div class="nav-header"><a href="" class="nav-header-link">Бисероплетение</a></div>
+      <ul class="nav-content">
+        <li class="nav-item">
+          <a href="" class="nav-link">Cхемы</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Идеи</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Магазин</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Статьи</a>
+        </li>
+      </ul>
+    </div>
+    <div class="nav-column">
+      <div class="nav-header"><a href="pages/Macrame.html" class="nav-header-link">Макраме</a></div>
+      <ul class="nav-content">
+        <li class="nav-item">
+          <a href="" class="nav-link">Схемы</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Видеоуроки</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Магазин</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Статьи</a>
+        </li>
+      </ul>
+    </div>
+    <div class="nav-column">
+      <div class="nav-header"><a href="pages/Embroidery.html" class="nav-header-link">Вышивание</a></div>
+      <ul class="nav-content">
+        <li class="nav-item">
+          <a href="" class="nav-link">Схемы</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Идеи</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Магазин</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Статьи</a>
+        </li>
+      </ul>
+    </div>
+    <div class="nav-column">
+      <div class="nav-header"><a href="" class="nav-header-link">Вязание</a></div>
+      <ul class="nav-content">
+        <li class="nav-item">
+          <a href="" class="nav-link">Схемы</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Видеоуроки</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Магазин</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Статьи</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
+</div> 
+`
+document.body.insertAdjacentHTML("afterbegin", navbar);
